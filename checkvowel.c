a350d98605f62c10ee8c2c28020bb2ed4cccd3bc
Hyperbhabani <bhabanisankarsahoo.itm.011@gmail.com>
2025-03-28 19:19:16 +0530

    Check a alphabet is vowel or not

---
+++ checkvowel.c
@@ -0,0 +1,32 @@
+#include "stdio.h"
+int main()
+{
+        char alphabet;
+        printf("Enter any  Small Alaphabet to check that is vowel or not:-");
+        scanf("%c",&alphabet);
+        if(alphabet=='a')
+        {
+            printf("The alphabet is vowel\n");
+        }
+        else if(alphabet=='e')
+        {
+            printf("The alphabet is a vowel");
+        }
+        else if(alphabet=='i')
+        {
+            printf("The alphabet is vowel\n");
+        }
+        else if(alphabet=='o')
+        {
+            printf("The alphabet is vowel\n");
+        }
+        else if(alphabet=='u')
+        {
+            printf("The alphabet is a vowel");
+        }
+        else
+        {
+            printf("The alphabet is not a vowel");
+        }
+
+}
