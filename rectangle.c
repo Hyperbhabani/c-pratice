b3d241527624435dba4cb29dbfe084b14805c698
Hyperbhabani <bhabanisankarsahoo.itm.011@gmail.com>
2025-03-28 19:34:53 +0530

    Find the area of the Rectangle

---
+++ rectangle.c
@@ -0,0 +1,11 @@
+#include "Stdio.h"
+int main()
+{
+    int l,b;
+    printf("Enter the Length of the rectangle\n");
+    scanf("%d",&l);
+    printf("Enter the breath of the rectangle\n");
+    scanf("%d",&b);
+    int Area=l*b;
+    printf("The area of the rectangle is:%d\n",Area);
+}
