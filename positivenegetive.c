42f966a5e4b3d1cebe2bc75ba4a83ea57fd3514a
Hyperbhabani <bhabanisankarsahoo.itm.011@gmail.com>
2025-03-29 18:04:54 +0530

    Check a number is positive or negetive

---
+++ positivenegetive.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+int main()
+{
+        int no;
+        printf("Enter any no:");
+        scanf("%d",&no);
+        if(no>=0)
+        {
+            printf("The no is positive(+ve) no:\n");
+        }
+        else
+        {
+            printf("The no is 
+                 negetive(-ve) no:\n");
+        }
+}
